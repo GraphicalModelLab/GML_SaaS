7986e3c67de822ffe04cf6ccc3b964fcdf10ca10
itomao <itomao@itomaos-MacBook-Pro.local>
2018-06-26 13:44:15 +0900

    added react-sticky version

--- ui/v0.1/src/component/graphLab/graphComponent/edgeDeletion.jsx
+++ ui/v0.1/src/component/graphLab/graphComponent/edgeDeletion.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Draggable from 'react-draggable';
 import NodePropertyView from './../graphProperty/nodePropertyView'
 
 
--- ui/v0.1/src/component/graphLab/graphProperty/nodePropertyView.jsx
+++ ui/v0.1/src/component/graphLab/graphProperty/nodePropertyView.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Draggable from 'react-draggable';
 import Modal from 'react-modal';
 import * as styles from './../../../css/structure.css';
 import NodeProperty from './nodeProperty';
