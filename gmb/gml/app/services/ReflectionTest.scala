743043f55a472dda5757a4a2397784da96396652
itomao <itomao@itomaos-MacBook-Pro.local>
2018-09-03 23:47:51 +0900

    added an example to use Kernel Density Estimation

--- gmb/gml/app/services/ReflectionTest.scala
+++ gmb/gml/app/services/ReflectionTest.scala
@@ -1,23 +1,36 @@
 package services
-import java.util.ServiceLoader
 
-import services.ModelSimpleCSV
-import services.ModelSimple2
-import services.Model
-import scala.reflect.runtime.universe._
-import scala.collection.JavaConverters._
 /**
   * Created by itomao on 8/15/18.
   */
 object ReflectionTest {
   def main(args: Array[String]): Unit = {
-    val mirror = runtimeMirror(this.getClass.getClassLoader)
+    var k = List[Double](
+        1,1,1,1,1,1,1,1,1,1,
+      2,2,2,2,2,2,2,2,
+      3,3,3,3,3,3,
+      4,4,4,4,
+      6,6,
+      8
+    )
+    var bandwidth = 2
+    println(densityEstimate(bandwidth,1,k))
+    println(densityEstimate(bandwidth,2,k))
+    println(densityEstimate(bandwidth,3,k))
+    println(densityEstimate(bandwidth,4,k))
+    println(densityEstimate(bandwidth,5,k))
+    println(densityEstimate(bandwidth,6,k))
+    println(densityEstimate(bandwidth,8,k))
 
-    val ws = (ServiceLoader load classOf[Model]).asScala
-    for (w <- ws) {
-      println(w.getModelName)
-      Console println s"Turn a ${w.getClass} by ${w.getClass.getName}"
-    }
   }
 
+  def densityEstimate(bandwidth: Double, input: Double, data: List[Double]) : Double = {
+    var output = 0;
+
+    data.map(f => GuassianKernel((f - input)/bandwidth)).sum/(data.length*bandwidth)
+  }
+
+  def GuassianKernel(data: Double): Double = {
+    return Math.exp(-data*data/2)/Math.sqrt(2*Math.PI);
+  }
 }
