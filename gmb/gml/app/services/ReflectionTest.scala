eac3fe58bbb4af6228e45a4f7ccffc6052521817
itomao <itomao@itomaos-MacBook-Pro.local>
2018-09-08 23:46:07 +0900

    maded Kernel Approche more effiencent

--- gmb/gml/app/services/ReflectionTest.scala
+++ gmb/gml/app/services/ReflectionTest.scala
@@ -1,26 +1,36 @@
 package services
 
+import org.apache.spark.mllib.linalg.Vectors
+
 /**
   * Created by itomao on 8/15/18.
   */
 object ReflectionTest {
   def main(args: Array[String]): Unit = {
-    var k = List[Double](
-        1,1,1,1,1,1,1,1,1,1,
-      2,2,2,2,2,2,2,2,
-      3,3,3,3,3,3,
-      4,4,4,4,
-      6,6,
-      8
-    )
-    var bandwidth = 2
-    println(densityEstimate(bandwidth,1,k))
-    println(densityEstimate(bandwidth,2,k))
-    println(densityEstimate(bandwidth,3,k))
-    println(densityEstimate(bandwidth,4,k))
-    println(densityEstimate(bandwidth,5,k))
-    println(densityEstimate(bandwidth,6,k))
-    println(densityEstimate(bandwidth,8,k))
+    val t1 = Vectors.dense(List[Double](120,202,22).toArray)
+    val t2 = Vectors.dense(List[Double](11,1,10).toArray)
+
+
+    val vectors = Vectors.dense((t1.toArray, t2.toArray).zipped.map(_ + _))
+
+    println(vectors.toArray.mkString(","));
+//    println(List[Double](0.5,0.2,1).toA)
+//    var k = List[Double](
+//        1,1,1,1,1,1,1,1,1,1,
+//      2,2,2,2,2,2,2,2,
+//      3,3,3,3,3,3,
+//      4,4,4,4,
+//      6,6,
+//      8
+//    )
+//    var bandwidth = 2
+//    println(densityEstimate(bandwidth,1,k))
+//    println(densityEstimate(bandwidth,2,k))
+//    println(densityEstimate(bandwidth,3,k))
+//    println(densityEstimate(bandwidth,4,k))
+//    println(densityEstimate(bandwidth,5,k))
+//    println(densityEstimate(bandwidth,6,k))
+//    println(densityEstimate(bandwidth,8,k))
 
   }
 
