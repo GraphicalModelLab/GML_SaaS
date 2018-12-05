4e1976de4407eabb40db97a5b6b4d269f9e1aa6f
itomao <itomao@itomaos-MacBook-Pro.local>
2018-12-05 21:33:01 +0900

    Remove sample_demo

--- sample_demo/AnimationSystem/src/main/java/org/graphicalmodellab/app/animationsystem/AnimationSystem.java
+++
@@ -1,8 +0,0 @@
-package org.graphicalmodellab.app.animationsystem;
-
-public class AnimationSystem {
-	public static void main(String[] args) {
-		  /** For executable Jar file **/
-		  new MotionViewer(1,10);
-	  }
-}
